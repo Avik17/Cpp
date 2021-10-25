a1170659cb5821865d898aa80d06d657a5fa6c42
Avinash <guptaavinash96@gmail.com>
2021-10-25 22:33:29 +0530

    deleted some codes

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-
-    for (int i = 0; i < n; i++)
-    {
-       for (int j  = 0; j < n ; j++)
-       {
-           if( n-j<=i+1){
-               cout<<n-j<<" ";
-           }
-           else cout<<"  ";
