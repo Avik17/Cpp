dbc90b601d636fc401f56b3b258eea6c1d808eda
Avinash <guptaavinash96@gmail.com>
2022-01-17 16:49:43 +0530

    STL added

---
+++ Functions/dsa/passing_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int[] array(int n){
+    int* ptr;
+    ptr= (int *) malloc(n*sizeof(int));
+    return ptr;
+
+    
+}
+    int main(){
+    int* A;
+    A=array(5);
+ 
+ 
+
+return 0;
+}
