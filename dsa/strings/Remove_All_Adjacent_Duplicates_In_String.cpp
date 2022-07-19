264bdc2da51441ab161c0b3e61ea91819374c25d
Avinash Kumar <guptaavinash96@gmail.com>
2022-07-19 22:25:29 +0530

    Remove All Adjacent Duplicates In String

---
+++ dsa/strings/Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+    int main(){
+    class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        ans = ans + s[0];
+        for (int i=1; i<s.length(); i++) {
+            if (s[i]==ans.back()) {
+                ans.pop_back();
+            }
+            else {
+                ans += s[i];
+            }
+        }
+        return ans;
+        
+    }
+};
+
+ 
+ 
+
+return 0;
+}
